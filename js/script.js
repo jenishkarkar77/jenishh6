11f3d45b86af5646aded277f4bbfb363dc8f127a
jenishkarkar77 <jenishkarkar77@gmail.com>
2023-08-08 14:43:02 +0530

    project-5

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    $('#slider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+});
+
+$(document).ready(function(){
+
+    $('#slider2').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+});
